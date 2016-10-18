b851b1da6cd5ba97e4011dfd97b60159061d01c6
oriportinho <ori.portinho@gmail.com>
2016-10-18 18:58:23 -0200

    Update sh config
    The tiniest little small bug ever

--- confs/inicial_config.sh
+++ confs/inicial_config.sh
@@ -2,12 +2,7 @@
 
 echo "install ambient system for Certifico-te"
 
-cp /root/certifico-te/certifico-te/cadastrar_certificado.php /var/www/html/index.php
-cp /root/certifico-te/certifico-te/validar_certificado.php /var/www/html/cadastrar/index.php
+cp /root/certifico-te/cadastrar_certificado.php /var/www/html/index.php
+cp /root/certifico-te/validar_certificado.php /var/www/html/cadastrar/index.php
 
 sudo service apache2 restart
-
-
-
-
-
