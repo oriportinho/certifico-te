d6d332a2d4d337dbadbcb3fa0f30f4f14fe86e64
douglasmcarmona <douglasmcarmona@gmail.com>
2016-10-25 11:29:24 -0200

    Update crud_cursos.php
    inserção do curso no banco de dados

--- crud_cursos.php
+++ crud_cursos.php
@@ -1,20 +1,28 @@
 <?php
+$curso = array();
+$curso['descricao'] = $_POST['nome_evento'];
+$curso['horario'] = $_POST['data_evento'];
+$curso['palestrante'] = $_POST['carga_horaria'];
+create($curso);
+header('Location: cadastrar_certificado.php');
+
 function create($curso) {
 	$query = "INSERT INTO Curso(DesCur, HorCur, NomPalCur)" .
 			 "VALUES (" . $curso->descricao . ", " . $curso->horario . ", " . $curso->palestrante . ");";
 	
-	if(!mysql_query($query)) echo "erro";
+	if(!mysql_query($query)) die("erro");
+	else 
 }
 
 function retrieve($nome) {
 	$query = "SELECT * FROM Curso WHERE DesCur = '" . $nome . "';";
-	if(!mysql_query($query)) echo "erro";
+	if(!mysql_query($query)) die("erro");
 }
 
 function update() {}
 
 function delete($nome) {
 	$query = "DELETE FROM Curso WHERE NomCur = '" . $curso . "';";
-	if(!mysql_query($query)) echo "erro";
+	if(!mysql_query($query)) die("erro");
 }
 ?>
