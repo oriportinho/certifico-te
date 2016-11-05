4bbbce8cc51a06437de09d86f40bdd482c045e83
douglasmcarmona <douglasmcarmona@gmail.com>
2016-11-05 13:37:11 -0200

    Update inicial_config.sh
    inserindo crud_cursos.php

--- confs/inicial_config.sh
+++ confs/inicial_config.sh
@@ -4,5 +4,6 @@ echo "install ambient system for Certifico-te"
 
 cp /root/certifico-te/cadastrar_certificado.php /var/www/html/index.php
 cp /root/certifico-te/validar_certificado.php /var/www/html/cadastrar/index.php
+cp /root/certifico-te/crud_crusos.php /var/www/html/crud_crusos.php
 
 sudo service apache2 restart
