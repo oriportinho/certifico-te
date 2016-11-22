7371bcec2384f4de49c54e924cdba15bbec22103
oriportinho <ori.portinho@gmail.com>
2016-11-22 18:48:38 -0200

    Teste em servidor

--- confs/install-server.sh
+++ confs/install-server.sh
@@ -3,7 +3,8 @@ apt install apache2 mysql-server php7.0 libapache2-mod-php7.0 php7.0-mysql -y
 
 mkdir /var/www/html
 mkdir /var/www/html/cadastrar
+mkdir /var/www/html/cadastrar/uploads
 
 chmod -R 755 /var/www
 
-
+apt install phpmyadmin
--- crud_curso.php
+++ crud_curso.php
@@ -12,17 +12,21 @@ $curso['descricao'] = $_POST['nome_evento'];
 $curso['horario'] = $_POST['data_evento'];
 $curso['palestrante'] = $_POST['carga_horaria'];
 create($curso, $conn);
-header('Location: index.php');
 
-query = "SELECT * FROM Pessoa";
-$stmt = $conn->prepare($query);
-$stmt->execute();
-$stmt->bind_result($cpf, $nome, $telefone, $email);
+$file_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]);
+print_r($_FILES);
+
+// query = "SELECT * FROM Pessoa";
+// $stmt = $conn->prepare($query);
+// $stmt->execute();
+// $stmt->bind_result($cpf, $nome, $telefone, $email);
 
 $sendgrid = new \SendGrid('fgXoUzchRnWsC5x94UThdA');
 
+/*
 while($stmt->fetch()) {
-	
+
 	$mailer = new SendGrid\EMail();
 	$mailer -> addTo($email)
 			-> setFrom('douglasmcarmona@gmail.com')
@@ -33,6 +37,9 @@ while($stmt->fetch()) {
 	$response = $sendgrid->client->mail()->send()->post($mailer);
 	var_dump($response);
 }
+*/
+
+// header('Location: index.php');
 
 function create($curso, $conn) {
         //$conn = new mysqli($servername, $username, $password, $dbname);
@@ -44,7 +51,7 @@ function create($curso, $conn) {
                          "VALUES (" . $curso['descricao'] . ", " . $curso['horario'] . ", " . $curso['palestrante'] . ");";
 
         if(!mysqli_query($query)) die("erro");
-	
+
         $conn->close();
 }
 
