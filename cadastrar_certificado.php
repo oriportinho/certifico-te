867532002d017418ad00bdf8a948e554eb217793
douglasmcarmona <douglasmcarmona@gmail.com>
2016-11-28 09:31:32 -0200

    Update cadastrar_certificado.php
    action do form

--- cadastrar_certificado.php
+++ cadastrar_certificado.php
@@ -49,7 +49,7 @@
 			<center>
 				<h1>Certifico - te</h1>
 				<br>
-				<form method="post">
+				<form method="post" action="crud_curso.php">
 					<table>
 						<tr>
 							<td>Nome do evento:</td>
